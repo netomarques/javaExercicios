3c088c0dc690f7511c81ee0b20fa65e774c5aacb
wilson.neto <wilson.neto@SETIN015.tce.am.gov.br>
2016-12-12 12:25:43 -0400

    Lista de Exercícios

--- javaExercicios/src/lista3/Exec_8.java
+++ javaExercicios/src/lista3/Exec_8.java
@@ -16,10 +16,16 @@ public class Exec_8 {
     public static void main(String args[]){
         
         int n; //numero de alunos na turma;
+        int m; //numero de homens na turma;
+        int f; //numero de mulheres na turma;
+        int repM; //numero de homens reprovados;
+        int repF; //numero de mulheres reprovadas;
         
         char sexo[];
         String[] aluno; //vetor de alunos;
         
+        double mediaM; //media da nota dos homens;
+        double mediaF; //media da nota das mulheres;
         double nota[][]; //vetor de notas, nota[*][] = indice do aluno;
                                             //nota[][*] = indice da nota, penultima posição referente a média e ultima numero de falta;
                          
@@ -32,13 +38,14 @@ public class Exec_8 {
         sexo = new char[n];
         nota = new double[n][5];
         
-        for(int i=0; i<=n-1; i++){
+        for(int i=0; i<n; i++){
             System.out.println("Nome do aluno: ");
             aluno[i] = sc.next();
             System.out.print("Sexo(M / F): ");
             sexo[i] = sc.next().charAt(0);
             
-            for(int j=0; j<3; j++){                System.out.print("Nota "+(j+1)+": ");
+            for(int j=0; j<3; j++){                
+                System.out.print("Nota "+(j+1)+": ");
                 nota[i][j] = sc.nextDouble();
             }
             
@@ -49,6 +56,38 @@ public class Exec_8 {
             
         }
         
+        
+        m = 0;
+        f = 0;
+        mediaM = 0.0;
+        mediaF = 0.0;
+        repM = 0;
+        repF = 0;
+        
+        for(int i=0; i<n; i++){
+            System.out.print("Aluno(a): "+aluno[i]);
+            if(nota[i][3]>=6){
+                System.out.println("Situação: APROVADO");
+                if(sexo[i] == 'M'){
+                    m = m+1;
+                    mediaM = mediaM + nota[i][3];
+                }else {
+                    f = f+1;
+                    mediaF = mediaF + nota[i][3];
+                }
+            }else{
+                System.out.println("Situação: REPROVADO");
+                if(sexo[i] == 'M'){
+                    m = m+1;
+                    mediaM = mediaM + nota[i][3];
+                    repM =  repM + 1;
+                }else{
+                    f = f+1;
+                    mediaF = mediaF + nota[i][3];
+                    repF =  repF + 1;
+                }
+            }
+        }
         
     }
 }
