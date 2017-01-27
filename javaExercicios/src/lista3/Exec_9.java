799e212b803ba7953d0c739c4c17d8abc52cff44
wilson.neto <wilson.neto@SETIN015.tce.am.gov.br>
2017-01-27 12:36:14 -0400

    Lista de Exercícios

--- javaExercicios/src/lista3/Exec_9.java
+++ javaExercicios/src/lista3/Exec_9.java
@@ -20,6 +20,9 @@ public class Exec_9 {
         ArrayList<String> sexos = new ArrayList<>();
         ArrayList<Double> salarios = new ArrayList<>();
         int idade = 0;
+        int qtdeHomem = 0;
+        int qtdeMulher = 0;
+        int qtdeMulher1000 = 0;
         String sexo;
         int maiorIdade = 0;
         int menorIdade = 0;
@@ -32,19 +35,23 @@ public class Exec_9 {
         idade = sc.nextInt();
         menorIdade = idade;
         
-        while(idade >= 0){
+        while(idade > 0){
             
             if(idade<menorIdade){
                 menorIdade = idade;
+            }
+            
+            if(idade>maiorIdade){
+                maiorIdade = idade;
             }
             idades.add(idade);
             
             do{
                 System.out.print("Sexo: ");
                 sexo = sc.next();
-            }while((sexo.equals('f')) || (sexo.equals('m')));
+            }while(sexo.equals('f'));//******
                     
-            sexos.add(sc.next());
+            sexos.add(sexo);
                     
             System.out.print("Salario: ");
             salarios.add(sc.nextDouble());
@@ -62,7 +69,26 @@ public class Exec_9 {
         }
         mediaSalario = mediaSalario/salarios.size();
         
+        for(int i=0; i<sexos.size();i++){
+            if(sexos.get(i).equals("f")){
+                qtdeMulher = qtdeMulher + 1;
+                
+                if(salarios.get(idade)<=1000){
+                    qtdeMulher1000 = qtdeMulher1000 + 1;
+                }
+                
+            }else{
+                
+                qtdeHomem = qtdeHomem + 1;
+                
+            }
+        }
         
+        System.out.println("Média dos salários: "+mediaSalario);
+        System.out.println("Maior idade: "+maiorIdade);
+        System.out.println("Menor idade: "+menorIdade);
+        System.out.println("Quantidade de Mulheres que recebem até R$1000,00: "+((qtdeMulher1000*100)/qtdeMulher)+ "%");
+        System.out.println("Quantidade de Homens: "+qtdeHomem);
     }
     
 }
